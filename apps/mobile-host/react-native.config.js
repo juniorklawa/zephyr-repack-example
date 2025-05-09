935112b5a5e32ef7782a318503795426922fc8ad
theGrep01 <denis.belotrserkovets@gmail.com>
2025-05-09 15:51:07 +0300

    feat: update dev deps

--- apps/mobile-host/react-native.config.js
+++ apps/mobile-host/react-native.config.js
@@ -1,13 +1,5 @@
 module.exports = {
   commands: require('@callstack/repack/commands/rspack'),
-  project: {
-    ios: {
-      sourceDir: './ios',
-    },
-    android: {
-      sourceDir: './android',
-    },
-  },
   assets: ['./assets/fonts'],
   dependencies: {
     'react-native-screen-corner-radius': {
