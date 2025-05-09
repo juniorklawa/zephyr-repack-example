f13a0e34a55529263051e71fd04294d887ffbeb2
theGrep01 <denis.belotrserkovets@gmail.com>
2025-05-09 15:41:37 +0300

    feat: specify sourceDir

--- apps/mobile-host/react-native.config.js
+++ apps/mobile-host/react-native.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   commands: require('@callstack/repack/commands/rspack'),
+  project: {
+    ios: {
+      sourceDir: './ios',
+    },
+    android: {
+      sourceDir: './android',
+    },
+  },
   assets: ['./assets/fonts'],
   dependencies: {
     'react-native-screen-corner-radius': {
